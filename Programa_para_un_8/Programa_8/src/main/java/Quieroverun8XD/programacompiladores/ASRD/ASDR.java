3399d0c2c1da90d09139c76cef69cb44f8c9ceaf
Ivantglz97 <ivantglz27@gmail.com>
2023-12-21 23:55:52 -0600

    Analizador Base sin arbol

--- Programa_para_un_8/Programa_8/src/main/java/Quieroverun8XD/programacompiladores/ASRD/ASDR.java
+++ Programa_para_un_8/Programa_8/src/main/java/Quieroverun8XD/programacompiladores/ASRD/ASDR.java
@@ -1,1 +1,22 @@
 
+package main.java.Quieroverun8XD.programacompiladores.ASRD;
+import main.java.Quieroverun8XD.programacompiladores.AnalizadorLexico.TipoToken;
+import  main.java.Quieroverun8XD.programacompiladores.AnalizadorLexico.Token;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ASDR implements Parser{
+
+    private int i = 0;
+    private Token preanalisis;
+    private final List<Token> tokens;
+    private boolean hayErrores = false;
+
+    public ASDR(List<Token> tokens){
+
+        this.tokens = tokens;
+        preanalisis = tokens.get(i);
+
+    }
