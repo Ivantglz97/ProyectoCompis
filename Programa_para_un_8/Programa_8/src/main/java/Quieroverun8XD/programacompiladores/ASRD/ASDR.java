7c279c00b137027b672f0abdb7a09bd3248fb5ee
Ivantglz97 <ivantglz27@gmail.com>
2023-12-21 23:59:05 -0600

    DECLARATIONS

--- Programa_para_un_8/Programa_8/src/main/java/Quieroverun8XD/programacompiladores/ASRD/ASDR.java
+++ Programa_para_un_8/Programa_8/src/main/java/Quieroverun8XD/programacompiladores/ASRD/ASDR.java
@@ -36,3 +36,54 @@ public class ASDR implements Parser{
         return false;
 
     }
+       //PROGRAM -> DECLARATION
+    private List<Statement> PROGRAM() {
+
+        List<Statement> stmts = new ArrayList<>();
+
+        if(hayErrores)
+            return null;
+
+        return DECLARATION(stmts);
+
+    }
+
+       /*Declaraciones*/
+    //DECLARATION -> FUN_DECL DECLARATION || VAR_DECL DECLARATION || STATEMENT DECLARATION || EMPTY
+    private List<Statement> DECLARATION(List<Statement> stmts) {
+
+        Statement statement;
+
+        if(hayErrores)
+            return null;
+
+        switch(preanalisis.getTipo()){
+            case FUN:
+                statement = FUN_DECL();
+                stmts.add(statement);
+                return DECLARATION(stmts);
+            case VAR:
+                statement = VAR_DECL();
+                stmts.add(statement);
+                return DECLARATION(stmts);
+            case BANG:
+            case MINUS:
+            case FALSE:
+            case TRUE:
+            case NULL:
+            case NUMBER:
+            case STRING:
+            case IDENTIFIER:
+            case LEFT_PAREN:
+            case PRINT:
+            case FOR:
+            case IF:
+            case RETURN:
+            case WHILE:
+            case LEFT_BRACE:
+                statement = STATEMENT();
+                stmts.add(statement);
+                return DECLARATION(stmts);
+        }
+        return stmts;
+    }
